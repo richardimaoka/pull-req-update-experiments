8b853ac5e4141fee872d4b2b4c3463b6e16596d0
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-20 07:49:43 +0900

    add more commands

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 )
 
 type Command struct {
@@ -24,17 +21,29 @@ EOF`, filename)
 
 func main() {
 	var commands []Command
+	commands = append(commands, Command{Command: "mkdir pull-req-update-experiments"})
+	commands = append(commands, Command{Command: "cd pull-req-update-experiments"})
+	commands = append(commands, Command{Command: "git init"})
+
+	commands = append(commands, Command{Command: "gh repo create pull-req-update-experiments --public --source=. --remote=origin"})
+
 	mainBranch := "developer"
-	filename := "pull-req-update-commit.txt"
+	filename := "pull-req-no-conflict.txt"
 	commands = append(commands, Command{Command: fmt.Sprintf(`git switch %s`, mainBranch)})
 	commands = append(commands, Command{Command: genAbcFile(filename)})
 	commands = append(commands, Command{Command: "git add --all"})
 	commands = append(commands, Command{Command: fmt.Sprintf(`git commit -m "%s"`, filename)})
 	commands = append(commands, Command{Command: fmt.Sprintf(`git push origin %s"`, mainBranch)})
-	bytes, err := json.Marshal(commands)
-	if err != nil {
-		log.Fatalf("json.Marshal error")
+
+	pr1branch := "pr-update-1"
+	commands = append(commands, Command{Command: fmt.Sprintf(`git switch %s`, pr1branch)})
+	commands = append(commands, Command{Command: fmt.Sprintf(`sed -i 's/a/aaaaa/' %s # ファイル中のaをaaaaaに置き換え`, filename)})
+	commands = append(commands, Command{Command: "git add --all"})
+	commands = append(commands, Command{Command: fmt.Sprintf(`git commit -m "%s"`, filename)})
+
+	// bytes, err := json.Marshal(commands)
+
+	for _, cmd := range commands {
+		fmt.Printf("%v\n", cmd.Command)
 	}
-	os.Stdout.Write(bytes)
-	fmt.Printf("%v\n", commands)
 }
--- scripts.sh
+++ scripts.sh
@@ -1,1 +1,22 @@
 #!/bin/sh
+
+cat << EOF > pull-req-squash-merge.txt
+a
+
+b
+
+c
+EOF
+git add --all
+git commit -m "create pull-req-squash-merge.txt"
+git push origin main
+
+
+git switch -c pr-squash-merge-1
+sed -i 's/a/aaaaa/' pull-req-squash-merge.txt # ファイル中のaをaaaaaに置き換え
+git add --all
+git commit -m "update a in pr-squash-merge-1"
+
+# GitHubにPull Requestを作成
+git push --set-upstream origin pr-squash-merge-1
+gh pr create --title pr-squash-merge-1 --body "" --base main --head pr-squash-merge-1

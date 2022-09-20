1c65a8293b2f8d66e0d3a7d3242a3fa65bd2f2f6
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-09-20 09:13:05 +0900

    replace Command with SingleCommand

--- main.go
+++ main.go
@@ -5,10 +5,6 @@ import (
 	"strings"
 )
 
-type Command struct {
-	Command string
-}
-
 type CommandStringer interface {
 	String() string
 }
@@ -45,39 +41,39 @@ EOF`, filename)
 }
 
 func main() {
-	var commands []Command
-	commands = append(commands, Command{Command: "#!/bin/sh\n"})
+	var commands []CommandStringer
+	commands = append(commands, &SingleCommand{Command: "#!/bin/sh\n"})
 
-	commands = append(commands, Command{Command: "# 準備: GitHub レポジトリの作成"})
+	commands = append(commands, &SingleCommand{Command: "# 準備: GitHub レポジトリの作成"})
 
-	commands = append(commands, Command{Command: "mkdir pull-req-update-experiments"})
-	commands = append(commands, Command{Command: "cd pull-req-update-experiments"})
-	commands = append(commands, Command{Command: "git init"})
+	commands = append(commands, &SingleCommand{Command: "mkdir pull-req-update-experiments"})
+	commands = append(commands, &SingleCommand{Command: "cd pull-req-update-experiments"})
+	commands = append(commands, &SingleCommand{Command: "git init"})
 
-	commands = append(commands, Command{Command: "gh repo create pull-req-update-experiments --public --source=. --remote=origin"})
+	commands = append(commands, &SingleCommand{Command: "gh repo create pull-req-update-experiments --public --source=. --remote=origin"})
 
 	mainBranch := "developer"
 	filename := "pull-req-no-conflict.txt"
-	commands = append(commands, Command{Command: "# 準備: GitHub レポジトリの作成"})
+	commands = append(commands, &SingleCommand{Command: "# 準備: GitHub レポジトリの作成"})
 
-	commands = append(commands, Command{Command: fmt.Sprintf(`git switch %s`, mainBranch)})
-	commands = append(commands, Command{Command: genAbcFile(filename)})
-	commands = append(commands, Command{Command: "git add --all"})
-	commands = append(commands, Command{Command: fmt.Sprintf(`git commit -m "%s"`, filename)})
-	commands = append(commands, Command{Command: fmt.Sprintf(`git push origin %s`, mainBranch)})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`git switch %s`, mainBranch)})
+	commands = append(commands, &SingleCommand{Command: genAbcFile(filename)})
+	commands = append(commands, &SingleCommand{Command: "git add --all"})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`git commit -m "%s"`, filename)})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`git push origin %s`, mainBranch)})
 
 	pr1branch := "pr-update-1"
-	commands = append(commands, Command{Command: "#  \n"})
-	commands = append(commands, Command{Command: fmt.Sprintf(`git switch %s`, pr1branch)})
-	commands = append(commands, Command{Command: fmt.Sprintf(`sed -i 's/a/aaaaa/' %s # ファイル中のaをaaaaaに置き換え`, filename)})
-	commands = append(commands, Command{Command: "git add --all"})
-	commands = append(commands, Command{Command: fmt.Sprintf(`git commit -m "%s"`, filename)})
-	commands = append(commands, Command{Command: fmt.Sprintf(`git push --set-upstream origin "%s"`, pr1branch)})
-	commands = append(commands, Command{Command: fmt.Sprintf(`gh pr create --title %s --body "" --base %s --head %s`, pr1branch, mainBranch, pr1branch)})
+	commands = append(commands, &SingleCommand{Command: "#  \n"})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`git switch %s`, pr1branch)})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`sed -i 's/a/aaaaa/' %s # ファイル中のaをaaaaaに置き換え`, filename)})
+	commands = append(commands, &SingleCommand{Command: "git add --all"})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`git commit -m "%s"`, filename)})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`git push --set-upstream origin "%s"`, pr1branch)})
+	commands = append(commands, &SingleCommand{Command: fmt.Sprintf(`gh pr create --title %s --body "" --base %s --head %s`, pr1branch, mainBranch, pr1branch)})
 
 	// bytes, err := json.Marshal(commands)
 
 	for _, cmd := range commands {
-		fmt.Printf("%v\n", cmd.Command)
+		fmt.Printf("%v\n", cmd.String())
 	}
 }
